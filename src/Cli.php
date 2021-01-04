fd9fa4c9027141d337418196e325eb8311501506
Anton Degoda <anton.degoda@renedesnumerique.fr>
2021-01-04 15:44:57 +0200

    Fixed linter errors

--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,18 @@
 <?php
+
 namespace php\project\lvl1\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
+
+/**
+ * Hello user function
+ */
 function run()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-}
+    line('Hello, %s!', $name);
 
+}//end run()
